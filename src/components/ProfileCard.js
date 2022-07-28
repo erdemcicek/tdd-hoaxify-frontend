49c8559d5d103755a7de4a6255dedf9927a75c50
erdemcicek <erdem25123@gmail>
2022-07-28 16:03:17 +0200

    until userupdate profile image front

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,8 +1,12 @@
 import React from "react";
 import ProfileImageWithDefault from "./ProfileImageWithDefault";
+import Input from "./Input";
+import ButtonWithProgress from "./ButtonWithProgress";
 
 const ProfileCard = (props) => {
   const { displayName, username, image } = props.user;
+
+  const showEditButton = props.isEditable && !props.inEditMode;
 
   return (
     <div className="card">
@@ -16,7 +20,49 @@ const ProfileCard = (props) => {
         />
       </div>
       <div className="card-body text-center">
-        <h4> {`${displayName}@${username}`} </h4>
+        {!props.inEditMode && <h4> {`${displayName}@${username}`} </h4>}
+        {props.inEditMode && (
+          <div className="mb-2">
+            <Input
+              value={displayName}
+              label={`Change Display Name for ${username}`}
+              onChange={props.onChangeDisplayName}
+            />
+          </div>
+        )}
+        {showEditButton && (
+          <button
+            className="btn btn-outline-success d-inline-flex"
+            onClick={props.onClickEdit}
+          >
+            {/* <span className="material-icons">edit</span>Edit */}
+            <i className="fas fa-user-edit" /> Edit
+          </button>
+        )}
+        {props.inEditMode && (
+          <div>
+            <ButtonWithProgress
+              className="btn btn-primary d-inline-flex"
+              onClick={props.onClickSave}
+              text={
+                <span>
+                  {/* <span className="material-icons">save</span>Save */}
+                  <i className="fas fa-save" /> Save
+                </span>
+              }
+              pendingApiCall={props.pendingUpdateCall}
+              disabled={props.pendingUpdateCall}
+            ></ButtonWithProgress>
+            <button
+              className="btn btn-danger d-inline-flex ms-1"
+              onClick={props.onClickCancel}
+              disabled={props.pendingUpdateCall}
+            >
+              {/* <span className="material-icons">cancel</span>Cancel */}
+              <i className="fas fa-window-close" /> Cancel
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/pages/UserPage.spec.js
+++ src/pages/UserPage.spec.js
@@ -1,7 +1,11 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { findByText, fireEvent, render, waitFor } from "@testing-library/react";
 import UserPage from "./UserPage";
 import * as apiCalls from "../api/apiCalls";
+import axios from "axios";
+import { Provider } from "react-redux";
+import configureStore from "../redux/configureStore";
+import { wait } from "@testing-library/user-event/dist/utils";
 
 const mockSuccessGetUser = {
   data: {
@@ -9,6 +13,15 @@ const mockSuccessGetUser = {
     username: "user1",
     displayName: "display1",
     image: "profile1.png",
+  },
+};
+
+const mockSuccessUpdateUser = {
+  data: {
+    id: 1,
+    username: "user1",
+    displayName: "display1-update",
+    image: "profile1-update.png",
   },
 };
 
@@ -20,6 +33,12 @@ const mockFailGetUser = {
   },
 };
 
+const mockFailUpdateUser = {
+  response: {
+    data: {},
+  },
+};
+
 const match = {
   params: {
     username: "user1",
@@ -27,7 +46,31 @@ const match = {
 };
 
 const setup = (props) => {
-  return render(<UserPage {...props} />);
+  const store = configureStore(false);
+  return render(
+    <Provider store={store}>
+      <UserPage {...props} />
+    </Provider>
+  );
+};
+
+beforeEach(() => {
+  localStorage.clear();
+  delete axios.defaults.headers.common["Authorization"];
+});
+
+const setUserOneLoggedInStorage = () => {
+  localStorage.setItem(
+    "hoax-auth",
+    JSON.stringify({
+      id: 1,
+      username: "user1",
+      displayName: "display1",
+      image: "profile1.png",
+      password: "P4ssword",
+      isLoggedIn: true,
+    })
+  );
 };
 
 describe("UserPage", () => {
@@ -62,6 +105,14 @@ describe("UserPage", () => {
       const spinner = queryByText("Loading...");
       expect(spinner).toBeInTheDocument();
     });
+    it("displays the edit button when loggedInUser matches to user in url", async () => {
+      setUserOneLoggedInStorage();
+      apiCalls.getUser = jest.fn().mockResolvedValue(mockSuccessGetUser);
+      const { queryByText, findByText } = setup({ match });
+      await findByText("display1@user1");
+      const editButton = queryByText("Edit");
+      expect(editButton).toBeInTheDocument();
+    });
   });
   describe("Lifecycle", () => {
     it("calls getUser when it is rendered", () => {
@@ -73,6 +124,167 @@ describe("UserPage", () => {
       apiCalls.getUser = jest.fn().mockResolvedValue(mockSuccessGetUser);
       setup({ match });
       expect(apiCalls.getUser).toHaveBeenCalledWith("user1");
+    });
+  });
+  describe("Profile Interactions", () => {
+    const setupForEdit = async () => {
+      setUserOneLoggedInStorage();
+      apiCalls.getUser = jest.fn().mockResolvedValue(mockSuccessGetUser);
+      const rendered = setup({ match });
+      const editButton = await rendered.findByText("Edit");
+      fireEvent.click(editButton);
+      return rendered;
+    };
+    const mockDelayedUpdateSuccess = () => {
+      return jest.fn().mockImplementation(() => {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve(mockSuccessUpdateUser);
+          }, 300);
+        });
+      });
+    };
+    it("displays edit layout when clicking edit button", async () => {
+      const { queryByText } = await setupForEdit();
+      expect(queryByText("Save")).toBeInTheDocument();
+    });
+    it("returns back to none edit mode after clicking cancel", async () => {
+      const { queryByText } = await setupForEdit();
+      const cancelButton = queryByText("Cancel");
+      fireEvent.click(cancelButton);
+      expect(queryByText("Edit")).toBeInTheDocument();
+    });
+    it("calls updateUser api when clicking save", async () => {
+      const { queryByText } = await setupForEdit();
+      apiCalls.updateUser = jest.fn().mockResolvedValue(mockSuccessUpdateUser);
+
+      const saveButton = queryByText("Save");
+      fireEvent.click(saveButton);
+
+      const userId = apiCalls.updateUser.mock.calls[0][0];
+
+      expect(userId).toBe(1);
+      //expect(apiCalls.updateUser).toHaveBeenCalledTimes(1);
+    });
+    it("calls updateUser api with request body having changed displayName", async () => {
+      const { queryByText, container } = await setupForEdit();
+      apiCalls.updateUser = jest.fn().mockResolvedValue(mockSuccessUpdateUser);
+
+      const displayInput = container.querySelector("input");
+      fireEvent.change(displayInput, { target: { value: "display1-update" } });
+
+      const saveButton = queryByText("Save");
+      fireEvent.click(saveButton);
+
+      const requestBody = apiCalls.updateUser.mock.calls[0][1];
+      expect(requestBody.displayName).toBe("display1-update");
+    });
+    it("returns to non edit mode after successful updateUser api call", async () => {
+      const { queryByText, findByText } = await setupForEdit();
+      apiCalls.updateUser = jest.fn().mockResolvedValue(mockSuccessUpdateUser);
+
+      const saveButton = queryByText("Save");
+      fireEvent.click(saveButton);
+      const editButtonAfterClickingSave = await findByText("Edit");
+
+      expect(editButtonAfterClickingSave).toBeInTheDocument();
+    });
+    it("returns to original displayName after its changed in edit mode but cancelled", async () => {
+      const { queryByText, container } = await setupForEdit();
+      const displayInput = container.querySelector("input");
+      fireEvent.change(displayInput, { target: { value: "display1-update" } });
+
+      const cancelButton = queryByText("Cancel");
+      fireEvent.click(cancelButton);
+
+      const originalDisplayText = queryByText("display1@user1");
+      expect(originalDisplayText).toBeInTheDocument();
+    });
+    it("returns to last updated displayName when display name is changed for another time but cancelled", async () => {
+      const { queryByText, findByText, container, queryByRole } =
+        await setupForEdit();
+
+      let displayInput = container.querySelector("input");
+      fireEvent.change(displayInput, { target: { value: "display1-update" } });
+      apiCalls.updateUser = jest.fn().mockResolvedValue(mockSuccessUpdateUser);
+
+      const saveButton = queryByRole("button", { name: "Save" });
+      fireEvent.click(saveButton);
+
+      const editButtonAfterClickingSave = await findByText("Edit");
+      fireEvent.click(editButtonAfterClickingSave);
+
+      displayInput = container.querySelector("input");
+      fireEvent.change(displayInput, {
+        target: { value: "display1-update-second-time" },
+      });
+
+      const cancelButton = queryByText("Cancel");
+      fireEvent.click(cancelButton);
+
+      const lastSavedData = container.querySelector("h4");
+
+      expect(lastSavedData).toHaveTextContent("display1-update@user1");
+    });
+    it("displays spinner when there is updateUser api call", async () => {
+      const { queryByText } = await setupForEdit();
+      apiCalls.updateUser = mockDelayedUpdateSuccess();
+
+      const saveButton = queryByText("Save");
+      fireEvent.click(saveButton);
+      const spinner = queryByText("Loading...");
+      expect(spinner).toBeInTheDocument();
+    });
+    it("disables save button when there is updateUser api call", async () => {
+      const { queryByText } = await setupForEdit();
+      apiCalls.updateUser = mockDelayedUpdateSuccess();
+
+      const saveButton = queryByText("Save");
+      fireEvent.click(saveButton);
+
+      expect(saveButton).toBeDisabled();
+    });
+    it("disables cancel button when there is updateUser api call", async () => {
+      const { queryByText } = await setupForEdit();
+      apiCalls.updateUser = mockDelayedUpdateSuccess();
+
+      const saveButton = queryByText("Save");
+      fireEvent.click(saveButton);
+
+      const cancelButton = queryByText("Cancel");
+
+      expect(cancelButton).toBeDisabled();
+    });
+    it("enables save button after updateUser api call success", async () => {
+      const { queryByText, container, findByText, queryByRole } =
+        await setupForEdit();
+      let displayInput = container.querySelector("input");
+      fireEvent.change(displayInput, { target: { value: "display1-update" } });
+      apiCalls.updateUser = jest.fn().mockResolvedValue(mockSuccessUpdateUser);
+
+      const saveButton = queryByRole("button", { name: "Save" });
+      fireEvent.click(saveButton);
+
+      const editButtonAfterClickingSave = await findByText("Edit");
+
+      fireEvent.click(editButtonAfterClickingSave);
+
+      const saveButtonAfterSecondEdit = queryByText("Save");
+
+      expect(saveButtonAfterSecondEdit).not.toBeDisabled();
+    });
+    it("enables save button after updateUser api call fails", async () => {
+      const { queryByRole, container } = await setupForEdit();
+      let displayInput = container.querySelector("input");
+      fireEvent.change(displayInput, { target: { value: "display1-update" } });
+      apiCalls.updateUser = jest.fn().mockRejectedValue(mockFailUpdateUser);
+
+      const saveButton = queryByRole("button", { name: "Save" });
+      fireEvent.click(saveButton);
+
+      await waitFor(() => {
+        expect(saveButton).not.toBeDisabled();
+      });
     });
   });
 });
